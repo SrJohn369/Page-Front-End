d6cf9147666805f2ea046f9f32e22dae128e8fe0
João Victor <106630200+SrJohn369@users.noreply.github.com>
2023-12-19 22:44:29 -0300

    modifiy

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,59 @@
 
+
 window.addEventListener("scroll", () => {
     let header = document.querySelector("#header")
     header.classList.toggle('rolagem-baixo', window.scrollY > 1)
 })
 
+// ============================= DISCO ==================================
+
+let currentAudio = null;
+let isPlaying = false;
+
+function toggleMusic() {
+    const vinylIcon = document.querySelectorAll('vinyl-icon');
+    console.log("entrou na funcção tuggle")
+
+    vinylIcon.forEach(() => {
+        if (!isPlaying) {
+            console.log("entrou no if")
+            isPlaying = true;
+            vinylIcon.style.display = 'inline-block';
+            startRotation();
+        } else {
+            isPlaying = false;
+            stopRotation();
+        }
+
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        currentAudio = chooseNextAudio();
+
+        if (isPlaying) {
+            currentAudio.play();
+        }
+    })
+}
+
+function chooseNextAudio() {
+    if (!currentAudio || currentAudio.id === 'audio4') {
+        return document.getElementById('audio1');
+    } else if (currentAudio.id === 'audio1') {
+        return document.getElementById('audio2');
+    } else if (currentAudio.id === 'audio2') {
+        return document.getElementById('audio3');
+    } else {
+        return document.getElementById('audio4');
+    }
+}
+
+function startRotation() {
+    document.getElementById('vinyl-icon').style.animationPlayState = 'running';
+}
+
+function stopRotation() {
+    document.getElementById('vinyl-icon').style.animationPlayState = 'paused';
+}
